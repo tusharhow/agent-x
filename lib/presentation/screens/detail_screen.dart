a6961507ca0212898b8c3d2f8d1b13e29f1ee238
Babish Shrestha <babish.shrestha@cgnet.com.np>
2023-12-14 12:16:25 +0545

    add: safeArea to details to fix overflow in top bar indicator

--- lib/presentation/screens/detail_screen.dart
+++ lib/presentation/screens/detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:agent_x/domain/entities/agent.dart';
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 
 class DetailScreen extends StatelessWidget {
@@ -6,97 +7,141 @@ class DetailScreen extends StatelessWidget {
   final Agent agent;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(16.0),
-          decoration: BoxDecoration(
-            gradient: agent.backgroundGradientColors.length > 1
-                ? LinearGradient(
-                    colors: agent.backgroundGradientColors
-                        .map((color) => Color(int.parse(color, radix: 16)))
-                        .toList(),
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                  )
-                : LinearGradient(
-                    colors: [
-                      Color(int.parse(agent.backgroundGradientColors[0],
-                          radix: 16)),
-                      Color(int.parse(agent.backgroundGradientColors[0],
-                          radix: 16)),
-                    ],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
+    final agentUltimate = agent.abilities.firstWhere(
+      (element) => element.slot == "Ultimate",
+      orElse: () => agent.abilities.last,
+    );
+    final Ability? agentPassive = agent.abilities.firstWhereOrNull(
+      (element) => element.slot == "Passive",
+    );
+    final agentAbilities = agent.abilities
+        .where((element) => element != agentUltimate && element != agentPassive)
+        .toList();
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              gradient: agent.backgroundGradientColors.length > 1
+                  ? LinearGradient(
+                      colors: agent.backgroundGradientColors
+                          .map((color) => Color(int.parse(color, radix: 16))
+                              .withOpacity(0.5))
+                          .toList(),
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    )
+                  : LinearGradient(
+                      colors: [
+                        Color(int.parse(agent.backgroundGradientColors[0],
+                            radix: 16)),
+                        Color(int.parse(agent.backgroundGradientColors[0],
+                            radix: 16)),
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Hero(
+                  tag: agent.name,
+                  child: Center(
+                    child: Image.network(
+                      agent.fullPortrait,
+                    ),
                   ),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Hero(
-                tag: agent.name,
-                child: Center(
-                  child: Image.network(
-                    agent.fullPortrait,
+                ),
+                const SizedBox(height: 16.0),
+                Text(
+                  agent.name,
+                  style: const TextStyle(
+                    fontSize: 26,
+                    fontFamily: 'Valorant',
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-              const SizedBox(height: 16.0),
-              Text(
-                agent.name,
-                style: const TextStyle(
-                  fontSize: 26,
-                  fontFamily: 'Valorant',
-                  fontWeight: FontWeight.bold,
+                const SizedBox(height: 8.0),
+                Row(
+                  children: [
+                    Text(
+                      agent.role,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                      ),
+                    ),
+                    const SizedBox(width: 8.0),
+                    Image.network(
+                      agent.roleDisplayIcon,
+                      height: 24.0,
+                    ),
+                  ],
                 ),
-              ),
-              const SizedBox(height: 8.0),
-              Text(
-                agent.role,
-                style: const TextStyle(
-                  fontSize: 16,
-                  color: Colors.black,
-                ),
-              ),
-              const SizedBox(height: 24.0),
-              const Text(
-                'Description:',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 8.0),
-              Text(
-                agent.description,
-                style: const TextStyle(
-                  fontSize: 16,
-                ),
-              ),
-              const SizedBox(height: 24.0),
-              const Text(
-                'Abilities:',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 8.0),
-              for (int i = 0; i < agent.abilities.length - 1; i++)
-                AgentDetailWidget(
-                  ability: agent.abilities[i],
-                ),
-              const SizedBox(height: 24.0),
-              const Text(
-                'Ultimate:',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 8.0),
-              AgentDetailWidget(ability: agent.abilities.last)
-            ],
+                const SizedBox(height: 24.0),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Description:',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 8.0),
+                    Text(
+                      agent.description,
+                      textAlign: TextAlign.justify,
+                      style: const TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    const SizedBox(height: 24.0),
+                    if (agentPassive != null)
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Passive:',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 8.0),
+                          AgentDetailWidget(ability: agentPassive!),
+                        ],
+                      ),
+                    const SizedBox(height: 24.0),
+                    const Text(
+                      'Abilities:',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 8.0),
+                    for (int i = 0; i < agentAbilities.length; i++)
+                      AgentDetailWidget(
+                        ability: agentAbilities[i],
+                      ),
+                    const SizedBox(height: 24.0),
+                    const Text(
+                      'Ultimate:',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 8.0),
+                    AgentDetailWidget(ability: agentUltimate),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
